25ea95ed4dde1eb01e107b507e3b7be4fb826074
XStarsssssss <shinehtetaung30507@gmail.com>
2025-11-30 15:53:25 +0630

    commit

--- src/services/AdminService.java
+++ src/services/AdminService.java
@@ -7,7 +7,7 @@ public class AdminService {
     public static boolean loginAsAdmin(String email, String password) {
         if (FakeDatabase.ADMIN_EMAIL.equalsIgnoreCase(email) && FakeDatabase.ADMIN_PASSWORD.equals(password)) {
             System.out.println("===========================================");
-            System.out.println("✔ services.AdminService login successful.");
+            System.out.println(" services.AdminService login successful.");
             System.out.println("===========================================");
             return true;
         }
